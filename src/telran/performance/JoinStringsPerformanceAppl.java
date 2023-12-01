62c947649ea3eee01fed4d2f07882438d7362ceb
mlipov <maxim.lipov@gmail.com>
2023-12-01 08:28:11 +0200

    231201 Update: application doesn't depend on the specific JoinString

--- src/telran/performance/JoinStringsPerformanceAppl.java
+++ src/telran/performance/JoinStringsPerformanceAppl.java
@@ -1,26 +1,33 @@
 package telran.performance;
 
+import java.lang.reflect.Constructor;
 import java.util.Arrays;
 
 import telran.text.*;
 
 public class JoinStringsPerformanceAppl {
+
 	private static final int N_STRINGS = 1000;
 	private static final int N_RUNS = 1000;
+	private static final String BASE_PACKAGE = "telran.text.";
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		String[] strings = getStrings();
-		PerformanceTest onBuilderTest = getTest("JoinStringsOnBuilder", strings, new JoinStringsOnBuilder());
-		PerformanceTest onStandardTest = getTest("JoinStringsOnStandard", strings, new JoinStringsOnStandard());
-		PerformanceTest onStringTest = getTest("JoinStringsOnString", strings, new JoinStringsOnString());
-		onBuilderTest.run();
-		onStandardTest.run();
-		onStringTest.run();
-	}
-
-	private static PerformanceTest getTest(String className, String[] strings, JoinStrings joinStrings) {
-		String testName = getTestName(className);
-		return new JoinStringsPerformanceTest(testName, N_RUNS, strings, joinStrings);
+		for (String className : args) {
+			try {
+				Class<JoinStrings> clazz = (Class<JoinStrings>) Class.forName(BASE_PACKAGE + className);
+				Constructor<JoinStrings> constructor = clazz.getConstructor();
+				JoinStrings joinStrings = constructor.newInstance();
+				JoinStringsPerformanceTest test = new JoinStringsPerformanceTest(getTestName(clazz.getSimpleName()),
+						N_RUNS, strings, joinStrings);
+				test.run();
+			} catch (ClassNotFoundException e) {
+				System.out.println(e);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	private static String getTestName(String className) {
@@ -32,4 +39,5 @@ public class JoinStringsPerformanceAppl {
 		Arrays.fill(res, "string");
 		return res;
 	}
+
 }
